b787c7a51df82907405f0b99b1f163a9210530dd
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-10-12 17:42:17 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,62 +1,44 @@
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
+from telegram.ext import *
+import telegram
+
+#from telegram.ext import Updater
+#from telegram.ext import  ContextTypes, CommandHandler
+#from telegram.ext import filters, MessageHandler, CommandHandler, ContextTypes
 
 BOT_TOKEN = '5778281282:AAHAPOtzeP7_qofFxkkb0KxgSJzhMarWn-Y'
-#bot = telegram.Bot(token= BOT_TOKEN)
-#pp.pprint(bot.get_me().to_dict())
-
-import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import InlineQueryHandler
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+CHAT_ID = -1001839847575
 
 
-async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = []
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    await context.bot.answer_inline_query(update.inline_query.id, results)
+def start_command(update, context):
+    name = update.message.chat.first_name
+    update.message.reply_text("Hello " + name)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+def image_downloader(update, context):
+    print (update.message['caption'])
+
+    file = update.message.photo[-1].file_id
+    obj = context.bot.get_file(file)
+    obj.download()   
+
+    update.message.reply_text("Image received")
 
 
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+def main():
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    #await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-    await context.bot.send_message(chat_id=-1001839847575, text=update.effective_message.text)
+    print("Started")
+    TOKEN = BOT_TOKEN
 
+    updater = Updater(TOKEN, use_context = True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start_command))
+    dp.add_handler(MessageHandler(Filters.photo, image_downloader))
+
+    #dp.bot.send_message(chat_id= -1001839847575, text='YO!!!!!')
+    dp.bot.send_photo(chat_id= -1001839847575, photo=open('images/file_0.jpg', 'rb'))
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
-    
-    start_handler = CommandHandler('start', start)
-    caps_handler = CommandHandler('caps', caps)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    inline_caps_handler = InlineQueryHandler(inline_caps)
-
-    application.add_handler(start_handler)
-    application.add_handler(caps_handler)
-    application.add_handler(echo_handler)
-    application.add_handler(inline_caps_handler)
-
-    application.run_polling()
+    main()
