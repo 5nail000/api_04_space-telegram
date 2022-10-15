88629272ef591b669bc4a279b381a4d4f815e9a7
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-10-16 02:45:02 +0300

    Fix bug

--- telegram_send_unlimit.py
+++ telegram_send_unlimit.py
@@ -6,8 +6,9 @@ from telegram_send_photo import *
 
 
 def send_telegram_unlimit(token, chat_id, sleep_time = 4, folder = 'images'):
+    
+    all_images = pick_all_imagefiles(folder)
     while True:
-        all_images = pick_all_imagefiles(folder)  
         for image in all_images:
             send_telegram_photo(token= token, chat_id= chat_id, image= image)
             time.sleep(60*60*float(sleep_time))
@@ -15,5 +16,5 @@ def send_telegram_unlimit(token, chat_id, sleep_time = 4, folder = 'images'):
 
 
 if __name__ == '__main__':
-    
+
     send_telegram_unlimit(token= BOT_TOKEN, chat_id= CHAT_ID, sleep_time= SLEEP_HOURS)
