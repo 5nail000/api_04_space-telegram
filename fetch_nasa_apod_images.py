3d467243ffabf5c76ab2cf02818c039cc21f317f
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-10-21 16:44:23 +0300

    fix media_type

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -13,12 +13,13 @@ def fetch_nasa_apod(folder = 'images', count = 30):
     response.raise_for_status()
 
     for image in response.json():
-        image_link = image['url']
-        parsed_link = urlparse(image_link)
-        file_name = f'nasa_apod_{image["date"]}_{parsed_link.path.split("/")[-1]}'
-        if parsed_link.hostname =='apod.nasa.gov':
-            download_image(image_link, file_name= file_name, folder= folder)
-            print(file_name)
+        if image['media_type'] == 'image': 
+            image_link = image['url']
+            parsed_link = urlparse(image_link)
+            file_name = f'nasa_apod_{image["date"]}_{parsed_link.path.split("/")[-1]}'
+            if parsed_link.hostname =='apod.nasa.gov':
+                download_image(image_link, file_name= file_name, folder= folder)
+                print(file_name)
     
     return True
 
