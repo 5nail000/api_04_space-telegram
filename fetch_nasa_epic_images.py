3a37521a5e25350a5983937f0f36add853bf9767
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-10-21 17:05:05 +0300

    cleaning code-text

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -13,10 +13,15 @@ def fetch_nasa_epic(natural= True, folder= 'images'):
     response.raise_for_status()
 
     for image in response.json():
+
         image_name = image['image']
-        image_date = f"{image['date'].split('-')[0]}/{image['date'].split('-')[1]}/{image['date'].split('-')[2][:2]}" 
+        file_name = f'nasa_{collection_type}_{image_name}.jpg'
+
+        image_year = f"{image['date'].split('-')[0]}"
+        image_month = f"{image['date'].split('-')[1]}"
+        image_day = f"{image['date'].split('-')[2][:2]}"
+        image_date = f"{image_year}/{image_month}/{image_day}" 
         image_link = f'https://epic.gsfc.nasa.gov/archive/{collection_type}/{image_date}/jpg/{image_name}.jpg'
-        file_name = f'nasa_{collection_type}_{image_link.split("/")[-1]}'
 
         download_image(image_link, file_name= file_name, folder= folder)
         print(file_name)
