30d0e5275eb5f071a443f151ddb26eed44488def
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-10-23 01:24:31 +0300

    add default argument

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,6 +1,7 @@
 import argparse
 import requests
 
+from time import sleep
 from base_functions import download_image
 
 
@@ -20,6 +21,9 @@ def fetch_spacex_images(launch_id, folder = 'images'):
     response.raise_for_status()
     images_sequense = response.json()['links']['flickr']['original']
 
+    if len(images_sequense) == 0:
+        return False
+
     for index, image in enumerate(images_sequense, 1):
         if len(str(len(images_sequense)))>len(str(index)): 
             zeroes = '0' * (len(str(len(images_sequense))) - len(str(index)))
@@ -37,7 +41,20 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("id", help="Launch id")
-    args = parser.parse_args()
-    print (f"{args.id} Launch. Getting links of images.")
 
-    if fetch_spacex_images(int(args.id)): print (f"Image seqene downloading is done")
+    id = 0
+    try:
+        args = parser.parse_args()
+        print (f"\n{args.id} Launch. Getting links of images.")
+        id = int(args.id)
+        
+    except:
+        print ('\nLets get first images (Launch ID= 12)')
+        id = 12
+
+    if fetch_spacex_images(id): 
+        print (f"\nImage seqene downloading is done\n")
+    else:
+        print (f"\nlaunch you have specified has no images\n")
+    
+    sleep(1)
