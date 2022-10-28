cdb4ec325e538233b8a6ff39ce60584c0b6eeb38
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-10-28 13:05:39 +0300

    refine variable (file_name)

--- base_functions.py
+++ base_functions.py
@@ -8,12 +8,12 @@ def pick_all_imagefiles(mypath = 'images'):
     return [f'{mypath}/{f}' for f in listdir(mypath) if isfile(join(mypath, f))]
 
 
-def download_image(link, file_name = None, folder = 'images'):
+def download_image(link, file_name = "", folder = 'images'):
     response = requests.get(link)
     response.raise_for_status()
 
     os.makedirs(folder, exist_ok= True)
-    if isinstance(file_name, type(None)):
+    if len(file_name) == 0:
         file_name = link.split('/')[-1]
 
     with open(f'{folder}/{file_name}', 'wb') as file:
