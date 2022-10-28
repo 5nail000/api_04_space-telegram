0e54dff7a947d850e2e831b011be40e085f57d9a
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-10-28 14:15:58 +0300

    fix pathlib

--- base_functions.py
+++ base_functions.py
@@ -1,11 +1,13 @@
-import os
 from os import listdir
 from os.path import isfile, join
+from pathlib import Path
+
 import requests
+import os
 
 
 def pick_all_imagefiles(mypath = 'images'):
-    return [f'{mypath}/{f}' for f in listdir(mypath) if isfile(join(mypath, f))]
+    return [ Path.cwd()/mypath/f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 
 def download_image(link, file_name = "", folder = 'images'):
@@ -16,6 +18,6 @@ def download_image(link, file_name = "", folder = 'images'):
     if len(file_name) == 0:
         file_name = link.split('/')[-1]
 
-    with open(f'{folder}/{file_name}', 'wb') as file:
+    with open(Path.cwd()/folder/file_name, 'wb') as file:
         file.write(response.content)
         return True
