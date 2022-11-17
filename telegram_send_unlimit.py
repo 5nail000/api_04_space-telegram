88eb4d9507447e63e2f28d72e07838fd3d7d6e83
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-11-17 18:01:02 +0300

    refine

--- telegram_send_unlimit.py
+++ telegram_send_unlimit.py
@@ -12,8 +12,8 @@ def send_telegram_unlimit(token, chat_id, sleep_time=4, folder='images'):
     all_images = pick_all_imagefiles(folder)
     while True:
         for image in all_images:
-                send_telegram_photo(token=token, chat_id=chat_id, image=image, url=False)
-                time.sleep(60*60*float(sleep_time))
+            send_telegram_photo(token=token, chat_id=chat_id, image=image, url=False)
+            time.sleep(60*60*float(sleep_time))
         random.shuffle(all_images)
 
 
