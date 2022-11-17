c8b26700b5d2ca26595b02481d79bcd948988872
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-11-17 16:32:17 +0300

    refine

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -5,9 +5,9 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 
-def get_nasa_apod_request(url, nasa_key, count = 30):
+def get_nasa_apod_request(url, nasa_key, count=30):
     params = {
-        'api_key' : nasa_key,
+        'api_key': nasa_key,
         'count': count
     }
     response = requests.get(url, params=params)
@@ -15,27 +15,24 @@ def get_nasa_apod_request(url, nasa_key, count = 30):
     return response.json()
 
 
-def fetch_nasa_apod(nasa_key, folder = 'images', count = 30):
-    
-    for image in get_nasa_apod_request('https://api.nasa.gov/planetary/apod', nasa_key, count):
-        if image['media_type'] == 'image': 
+def fetch_nasa_apod(nasa_key, folder='images', count=30):
+
+    api_url = 'https://api.nasa.gov/planetary/apod'
+    for image in get_nasa_apod_request(api_url, nasa_key, count):
+        if image['media_type'] == 'image':
             image_link = image['url']
             parsed_link = urlparse(image_link)
-            file_name = f'nasa_apod_{image["date"]}_{parsed_link.path.split("/")[-1]}'
-            if parsed_link.hostname =='apod.nasa.gov':
-                download_image(image_link, file_name= file_name, folder= folder)
+            file_name = parsed_link.path.split("/")[-1]
+            file_name = f'nasa_apod_{image["date"]}_{file_name}'
+            if parsed_link.hostname == 'apod.nasa.gov':
+                download_image(image_link, file_name=file_name, folder=folder)
                 print(file_name)
-    
+
     return True
 
 
 if __name__ == '__main__':
-    
-    load_dotenv()
 
-    try:
-        nasa_key = os.environ['NASA_KEY']
-    except KeyError as _ex: 
-        print (f'KeyError: {_ex}')
-    else:
-        fetch_nasa_apod(nasa_key)
+    load_dotenv()
+    nasa_key = os.environ['NASA_KEY']
+    fetch_nasa_apod(nasa_key)
